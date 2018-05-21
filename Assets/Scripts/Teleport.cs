a2b3338c009144337aece7e14816edfc3b24094f
Sabarish2003 <gnasa@utschools.ca>
2018-05-20 23:45:12 -0400

    Initial commit

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    private Camera _camera;
+
+    void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+
+
+    }
+
+    private void Shoot()
+    {
+        // shoot a raycast from the center of our screen
+        Ray ray = _camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit; // output variable to get what we collided against
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform != null)
+            {
+                // set our location to the point we hit
+                Vector3 newLocation = new Vector3(hit.point.x, 1, hit.point.z);
+                _camera.transform.position = newLocation;
+                newLocation = _camera.transform.position;
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/gameLogic.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class gameLogic : MonoBehaviour {
+
+    public int keyCount;
+    public Text countText;
+    public GameObject countDisplay;
+    public GameObject winningText;
+    public GameObject player;
+    public VideoPlayer howtoplay;
+    public GameObject videoObject;
+    public GameObject threetags;
+    public GameObject button;
+
+    // Use this for initialization
+    void Start () {
+        keyCount = 0;
+        countText.text = keyCount.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        string sceneName = currentScene.name;
+
+        if (sceneName == "Main")
+        {
+            if (keyCount == 5)
+            {
+                countDisplay.SetActive(false);
+                winningText.SetActive(true);
+                keyCount = 0;
+            }
+        }
+
+        if (sceneName == "Island")
+        {
+            if (keyCount == 5)
+            {
+                countDisplay.SetActive(false);
+                winningText.SetActive(true);
+                keyCount = 0;
+            }
+        }
+
+    }
+
+    public void PlusOne() {
+        keyCount = keyCount + 1;
+        countText.text = keyCount.ToString();
+    }
+
+    public void Click1()
+    {
+        SceneManager.LoadScene("Island");
+    }
+
+    public void Click2()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Beginner()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Advanced()
+    {
+        SceneManager.LoadScene("Island");
+    }
+
+    public void HowtoPlay()
+    {
+        videoObject.SetActive(true);
+        howtoplay.Play();
+        threetags.SetActive(false);
+        button.SetActive(true);
+    }
+
+    public void VideoClick()
+    {
+        videoObject.SetActive(false);
+        threetags.SetActive(true);
+        button.SetActive(false);
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("IntroductionAdvanced");
+    }
+}
